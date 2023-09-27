f6396c2b495be908f6a08d672fb74cbd29f17d2a
dahdouhMouad <mouaddhd10@gmail.com>
2023-09-27 12:39:56 +0100

    Initial Commit

---
+++ ReminderSwiftUI/ReminderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderSwiftUIApp.swift
+//  ReminderSwiftUI
+//
+//  Created by Macbook Pro on 27/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReminderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
